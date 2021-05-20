e1a04c184cf6fdf9f84dfa1cbc2c96ad0383c9d1
Zuleyma Castillo <59371009+Zuleyma77@users.noreply.github.com>
2021-05-19 22:52:02 -0700

    Add files via upload

---
+++ .script.js
@@ -0,0 +1,16 @@
+function numberChecking(number 1, number 2 ){
+    if(number1 >100){
+      console.log{"number 1 has manyy digits!";}
+    }
+if(number 1 <0){console.log{"Number 1 is nagative number!"};
+}
+
+if (number 2 <20){
+  Concole.log("number 2 is not a very high number");
+}
+
+if(number2 >50){console.log("number 2 is high number");
+}
+
+
+}
